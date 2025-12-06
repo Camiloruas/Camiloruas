9119b325b07bb075bebe825d0076d2378295c530
Camilo Ruas <miloruas@gmail.com>
2025-12-06 20:24:25 -0300

    feat: add WebDeveloper class with basic properties and method

---
+++ camilo.js
@@ -0,0 +1,11 @@
+class WebDeveloper {
+  constructor() {
+    this.user = "Camiloruas";
+    this.email = "miloruas@gmail.com";
+    this.skills = ["JS", "Frontend", "Backend"];
+  }
+  code() {
+    return "Building the future...";
+  }
+}
+const me = new WebDeveloper();
